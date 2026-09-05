718162ba7c68b98b88eb89384df2653cb979ea9a
LAPTOP-MER9U0J9\Nirbhay Dhaked <nirbhaydhaked@gmail.com>
2026-09-05 14:07:42 +0530

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RoleAndAdminSeeder::class);
+        //$this->call(RoleAndAdminSeeder::class);
         $this->call(QuizLevelSeeder::class);
         $this->call(QuizTechnologySeeder::class);
         $this->call(QuizQuestionSeeder::class);
